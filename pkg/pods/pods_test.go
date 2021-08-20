61da9b81c63d153c852b22a9842f534948810211
Tomba Leishangthem <tleishan@gmail.com>
2021-08-20 12:59:02 -0700

    Added terminated, completed and time elaspsed(TODO) test cases

--- pkg/pods/pods_test.go
+++ pkg/pods/pods_test.go
@@ -10,30 +10,35 @@ import (
 	"time"
 )
 
+func newPod(name, namespace string, phase v1.PodPhase) *v1.Pod {
+	pod := v1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: "default",
+			Labels: map[string]string{
+				"app": "intuit",
+			},
+		},
+		Status: v1.PodStatus{
+			Phase:     v1.PodRunning,
+			Reason:    "",
+			StartTime: &metav1.Time{Time: time.Now()},
+		},
+	}
+	return &pod
+}
+
 func TestPodLister_GetTerminatingPods(t *testing.T) {
 	mockLister := &podLister{
 		clientSet: fake.NewSimpleClientset(),
 	}
 	podName := "pod-terminating"
-	pod := v1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      podName,
-			Namespace: "default",
-			Labels: map[string]string{
-				"app": "intuit",
-			},
-			DeletionTimestamp: &metav1.Time{Time: time.Now()},
-		},
-		Status: v1.PodStatus{
-			Phase:     "Running",
-			Reason:    "",
-			StartTime: &metav1.Time{Time: time.Now()},
-		},
-	}
+	pod := newPod(podName, "default", v1.PodRunning)
+	pod.ObjectMeta.DeletionTimestamp = &metav1.Time{Time: time.Now()}
 	_, err := mockLister.clientSet.
 		CoreV1().
 		Pods("default").
-		Create(context.TODO(), &pod, metav1.CreateOptions{})
+		Create(context.TODO(), pod, metav1.CreateOptions{})
 	if err != nil {
 		panic(err)
 	}
@@ -41,3 +46,49 @@ func TestPodLister_GetTerminatingPods(t *testing.T) {
 	assert.True(t, len(result) == 1, "Only one pod expected in terminating state")
 	assert.Equal(t, result[0].Name, podName, "Unexpected pod name")
 }
+
+func TestPodLister_GetCompletedPods(t *testing.T) {
+	mockLister := &podLister{
+		clientSet: fake.NewSimpleClientset(),
+	}
+	podName := "pod-completed"
+	pod := newPod(podName, "default", v1.PodSucceeded)
+	_, err := mockLister.clientSet.
+		CoreV1().
+		Pods("default").
+		Create(context.TODO(), pod, metav1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+	result := mockLister.get("default", nil, nil)
+	assert.True(t, len(result) == 1, "Only one pod expected in terminating state")
+	assert.Equal(t, result[0].Name, podName, "Unexpected pod name")
+}
+
+/*
+func TestPodLister_GetTimeElapsedPods(t *testing.T) {
+	mockLister := &podLister{
+		clientSet: fake.NewSimpleClientset(),
+	}
+	podName := "pod-elapsed"
+	minutes := 10
+	elapsedDuration := time.Minute * time.Duration(minutes)
+	pod := newPod(podName, "default", v1.PodSucceeded)
+	pod.ObjectMeta.SetCreationTimestamp(metav1.Time{
+		Time: time.Now().Truncate(elapsedDuration),
+	})
+	fmt.Println(time.Now())
+	fmt.Println(pod.ObjectMeta.CreationTimestamp.Time)
+	_, err := mockLister.clientSet.
+		CoreV1().
+		Pods("default").
+		Create(context.TODO(), pod, metav1.CreateOptions{})
+	if err != nil {
+		panic(err)
+	}
+	minutes++
+	result := mockLister.get("default", nil, &minutes)
+	assert.True(t, len(result) == 1, "Only one pod expected in terminating state")
+	assert.Equal(t, result[0].Name, podName, "Unexpected pod name")
+}
+*/
